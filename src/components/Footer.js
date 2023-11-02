f2fa033af76526983b4cc1859995dc80dc5da1ff
Olaf-Tarkowski <tarkowski.software@gmail.com>
2023-11-02 16:25:36 +0100

    Well done footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,32 @@
+import styled from "styled-components";
+import logo from "../resources/images/logo.png";
 
-const Footer  = () => {
-  return(
-    <p>
-      hej
-    </p>
-  )
+const Container = styled.main`
+  background: #3a3a3a;
+`;
+
+const Section = styled.section`
+  padding: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+`;
+
+const Logo = styled.img`
+  height: 75px;
+  padding-right: 15px;
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      <Section>
+        <Logo src={logo} />
+        COPYRIGHT © 2023 POWERLAND - RODZINNA STREFA ZABAWY
+      </Section>
+    </Container>
+  );
 };
 
 export default Footer;
