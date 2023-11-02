a6e11d94aa98f7a5c9986c040177bec97fc81167
Olaf-Tarkowski <tarkowski.software@gmail.com>
2023-11-02 17:58:40 +0100

    PictureSection done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,39 @@
+import styled from "styled-components";
+import powerland_above from "../resources/images/powerland_above.jpeg";
+import logo from "../resources/images/logo.png";
+
+const Main = styled.main``;
+
+const PictureSection = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Picture = styled.img`
+  max-width: 100%;
+  background-position: center;
+`;
+
+const Logo = styled.img`
+  position: absolute;
+  margin-bottom: 50px;
+  max-height: 200px;
+  transition: 0.9s;
+  &:hover {
+    transform: scale(1.25);
+  }
+`;
+
 const Home = () => {
-  return(
-    <>
-    Home
-    </>
-  )
-}
+  return (
+    <Main>
+      <PictureSection>
+        <Picture src={powerland_above}></Picture>
+        <Logo src={logo} />
+      </PictureSection>
+    </Main>
+  );
+};
 
 export default Home;
