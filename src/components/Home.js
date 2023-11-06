a7f893b4d2ac787d98377e48b1dc9761735b8e9d
Olaf-Tarkowski <tarkowski.software@gmail.com>
2023-11-06 22:35:06 +0100

    add OpeningHoursCard

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,22 @@
 import styled from "styled-components";
 import powerland_above from "../resources/images/powerland_above.jpeg";
 import logo from "../resources/images/logo.png";
+import openingHours from "../resources/images/opening_hours.jpg";
+import { useState } from "react";
+import Fade from "react-reveal/Fade";
 
 const Main = styled.main``;
 
 const PictureSection = styled.section`
   display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const OpeningHours = styled.section`
+  padding: 20px 0;
+  display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
@@ -25,13 +36,50 @@ const Logo = styled.img`
   }
 `;
 
+const Button = styled.button`
+  font-family: "Atma", cursive;
+  padding: 10px 20px;
+  letter-spacing: 1.5px;
+  background-color: #cc2e72;
+  border-color: #02010100;
+  border-radius: 10px 10px 10px 10px;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.5);
+  transition: 0.9s;
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
+
+const StyledCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const OpeningHoursCard = styled.img`
+  height: auto;
+  max-width: 50%;
+`;
+
 const Home = () => {
+  const [visible, setVisible] = useState(false);
   return (
     <Main>
       <PictureSection>
         <Picture src={powerland_above}></Picture>
         <Logo src={logo} />
       </PictureSection>
+      <OpeningHours>
+        Zapraszamy codziennie !
+        <Fade collapse when={visible}>
+          <StyledCard>
+            <OpeningHoursCard src={openingHours} />
+          </StyledCard>
+        </Fade>
+        <Button onClick={() => setVisible(!visible)}>
+          {visible == true ? "SCHOWAJ" : "POKAŻ"} GODZINY OTWARCIA
+        </Button>
+      </OpeningHours>
     </Main>
   );
 };
