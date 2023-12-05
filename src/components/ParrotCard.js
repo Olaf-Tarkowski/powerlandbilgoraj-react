817c7977c668555b469faf5d9c43e1af56ab29bc
Olaf-Tarkowski <tarkowski.software@gmail.com>
2023-12-05 22:31:56 +0100

    Add regulations with fade

--- src/components/ParrotCard.js
+++ src/components/ParrotCard.js
@@ -10,6 +10,8 @@ import Parrot5 from "../resources/images/Parrot/Parrot5.jpg";
 import Parrot6 from "../resources/images/Parrot/Parrot6.jpg";
 import Parrot7 from "../resources/images/Parrot/Parrot7.jpg";
 import Parrot8 from "../resources/images/Parrot/Parrot8.jpg";
+import Regulations from "../resources/images/Parrot/Regulations.jpg";
+import { useState } from "react";
 
 const TitleText = styled.div`
   margin: auto;
@@ -27,6 +29,52 @@ const Line = styled.div`
   height: 2px;
   margin-bottom: 15px;
   background: #cc2e72;
+`;
+
+const OpeningHoursSection = styled.section`
+  padding: 20px 20px;
+  color: #cc2e72;
+  font-size: 40px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+`;
+
+const FontBorder = styled.div`
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+  letter-spacing: 1px;
+  word-spacing: 4px;
+`;
+
+const StyledCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Button = styled.button`
+  font-family: "Atma", cursive;
+  font-size: 15px;
+  font-weight: 500;
+  padding: 10px 25px;
+  letter-spacing: 1.5px;
+  color: #fff;
+  background-color: #cc2e72;
+  border-color: #02010100;
+  border-radius: 10px 10px 10px 10px;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.5);
+  transition: 0.8s;
+  &:hover {
+    transform: scale(1.15);
+  }
+`;
+
+const OpeningHoursCard = styled.img`
+  height: auto;
+  max-width: 1140px;
+  width: 100%;
 `;
 
 const Container = styled.div`
@@ -54,6 +102,7 @@ const ColCard = styled(Col)`
 `;
 
 const ParrotCard = () => {
+  const [visible, setVisible] = useState(false);
   return (
     <>
       <Fade right duration={2000}>
@@ -64,6 +113,21 @@ const ParrotCard = () => {
       <Fade left duration={2000}>
         <Line />
       </Fade>
+
+      <OpeningHoursSection>
+          <FontBorder>
+            <ColorfulText text="JUŻ OTWARTA - ZAPRASZAMY!" />
+          </FontBorder>
+          <Fade collapse when={visible}>
+            <StyledCard>
+              <OpeningHoursCard src={Regulations} />
+            </StyledCard>
+          </Fade>
+          <Button onClick={() => setVisible(!visible)}>
+            {visible === true ? "SCHOWAJ" : "POKAŻ"} REGULAMIN
+          </Button>
+        </OpeningHoursSection>
+
       <Container>
         <Fade duration={2000}>
         <RowCard>
